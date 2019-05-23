fcc7635e239ccac22522e6bfd99b8f33dc8dc16c
Victor Bessonov <victor.a.bessonov@outlook.com>
2019-05-23 15:53:14 +0400

    *.mk: support aosp oreo rom

---
+++ aosp_star2lte.mk
@@ -0,0 +1,14 @@
+$(call inherit-product, device/samsung/star2lte/full_star2lte.mk)
+
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Set Boot Animination Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_star2lte
+
+BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:8.0.0/R16NW/G965FXXU1ARCC:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRIVATE_BUILD_DESC="star2ltexx-user 8.0.0 R16NW G965FXXU1ARCC release-keys"
